c73a1b15cb84aba1d5c0d36cbea77405d1ad257b
Azamat Jauysh <79762730+Jauysh@users.noreply.github.com>
2024-03-18 10:54:18 +0200

    numpy

--- streamlit_playground.py
+++ streamlit_playground.py
@@ -5,6 +5,7 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
@@ -13,3 +14,9 @@ df = pd.DataFrame({
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(df)
+
+
+
+st.write("Numpy usage:")
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
