44ce1dfcdb85d5178a0545910910973d3174a05b
Azamat Jauysh <79762730+Jauysh@users.noreply.github.com>
2024-03-18 10:55:44 +0200

    highlights

--- streamlit_playground.py
+++ streamlit_playground.py
@@ -7,10 +7,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
+
+
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(df)
@@ -20,3 +24,13 @@ st.write(df)
 st.write("Numpy usage:")
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
+
+
+
+
+st.write("Numpy highlights sths:")
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
