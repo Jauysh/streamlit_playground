eb0ace9d54e5a72f5acfceab56c71faaff16daa8
Azamat Jauysh <79762730+Jauysh@users.noreply.github.com>
2024-03-18 10:58:08 +0200

    table

--- streamlit_playground.py
+++ streamlit_playground.py
@@ -34,3 +34,10 @@ dataframe = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
+
+
+st.write("Table usage:")
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
