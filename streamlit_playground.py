5f6346ee5740705a793a46aa73cb956c1daae4f5
Azamat Jauysh <79762730+Jauysh@users.noreply.github.com>
2024-03-18 11:05:24 +0200

    input key

--- streamlit_playground.py
+++ streamlit_playground.py
@@ -62,3 +62,9 @@ st.map(map_data)
 st.write("Slider usage:")
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+
+st.write("User Input usage:")
+st.text_input("Your name", key="name")
+# You can access the value at any point with:
+st.session_state.name
